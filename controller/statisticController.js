fba2c7d0768c5e79ea3bee65b2bd500f1cbbe985
kuluong2000 <tranhuuluong156200@gmail.com>
2022-11-29 16:31:17 +0700

    29/11/2022

--- controller/statisticController.js
+++ controller/statisticController.js
@@ -12,28 +12,37 @@ exports.statisPatientByMonth = catchAsync(async (req, res, next) => {
         day: { $dayOfMonth: '$date' },
       },
     },
+
     {
       $match: {
         year: Number(year),
         month: Number(month),
       },
     },
+
     {
       $group: {
         _id: '$day',
-        booking: { $push: '$$ROOT' },
+        // booking: { $push: '$$ROOT' },
         total: { $sum: 1 },
       },
     },
     {
+      $sort: { _id: 1 },
+    },
+    {
       $project: {
-        _id: 0,
+        _id: 1,
         day: '$_id',
-        booking: 1,
+        // booking: 1,
         total: 1,
       },
     },
   ]);
 
   res.json(stats);
+  // res.status(200).json({
+  //   status: 'success',
+  //   stats,
+  // });
 });
