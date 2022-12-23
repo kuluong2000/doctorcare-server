3d4aa79491789d7cf681d00a59d73290b3226a06
kuluong2000 <tranhuuluong156200@gmail.com>
2022-12-23 15:09:29 +0700

    x

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
-const app = require("./index");
+dotenv.config({ path: './config.env' });
+const app = require('./index');
 
-const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     autoIndex: true, //make this also true
   })
-  .then(() => console.log("DB connection successful!"))
-  .catch((err) => console.log("error"));
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.log('error'));
 
 let port = process.env.PORT || 3030;
-app.listen(port, () => {
+app.listen(5000, () => {
   console.log(`App running on port ${port}...`);
 });
